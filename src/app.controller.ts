fa9fdd2bec56ba5f0f91c1ff3643e9e82f22ea4a
huuduynvc <nguyenhuuduynvc@gmail.com>
2022-08-11 16:22:57 +0700

    roleguard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,6 +2,9 @@ import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
 import { AuthService } from './auth/auth.service';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { Role } from './enums/role.enum';
+import { Roles } from './role/roles.decorator';
+import { RolesGuard } from './role/roles.guard';
 
 @Controller()
 export class AppController {
@@ -13,6 +16,8 @@ export class AppController {
     return this.authService.login(req.user);
   }
 
+  @UseGuards(RolesGuard)
+  @Roles(Role.User)
   @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -16,7 +16,7 @@ import { LocalStrategy } from './local.strategy';
       signOptions: { expiresIn: '60s' },
     }),
   ],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService, JwtStrategy, LocalStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
